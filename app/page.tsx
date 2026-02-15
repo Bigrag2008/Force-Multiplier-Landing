de6fbd630db41cc19fe12e80c76553e0d4c61d15
Bigrag2008 <lamontrags12@gmail.com>
2026-02-15 18:15:13 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
+import Hero from './components/Hero'
+import Instructor from './components/Instructor'
+import Modules from './components/Modules'
+import Problem from './components/Problem'
+import SocialProof from './components/SocialProof'
+import Testimonials from './components/Testimonials'
+import FAQ from './components/FAQ'
+import FinalCTA from './components/FinalCTA'
+import Footer from './components/Footer'
+import LeadMagnet from './components/LeadMagnet'
+
 export default function Home() {
   return (
     <main>
